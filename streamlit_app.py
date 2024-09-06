ad83ac815e90198dfed8618cc1da960579700bce
Aditya Gupta <179336367+Aditya-Gupta-ctrl@users.noreply.github.com>
2024-09-06 17:26:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,10 @@
 import streamlit as st
 
-# Create a tab bar
-tab1, tab2, tab3, tab4, tab5 = st.tabs(["Tab 1", "Tab 2", "Tab 3", "Tab 4", "Tab 5"])
+import streamlit_antd_components as sac
 
-# Tab 1
-with tab1:
-    st.header("Tab 1")
-    st.write("This is tab 1")
-
-# Tab 2
-with tab2:
-    st.header("Tab 2")
-    st.write("This is tab 2")
-
-# Tab 3
-with tab3:
-    st.header("Tab 3")
-    st.write("This is tab 3")
-
-# Tab 4
-with tab4:
-    st.header("Tab 4")
-    st.write("This is tab 4")
-
-# Tab 5
-with tab5:
-    st.header("Tab 5")
-    st.write("This is tab 5")
+sac.tabs([
+    sac.TabsItem(label='apple', tag="10"),
+    sac.TabsItem(icon='google'),
+    sac.TabsItem(label='github', icon='github'),
+    sac.TabsItem(label='disabled', disabled=True),
+], align='center', size='lg', variant='outline', color='grape', use_container_width=True)
