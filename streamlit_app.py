f6591cfd719eca83f711823e9efae276823f14a5
Aditya Gupta <179336367+Aditya-Gupta-ctrl@users.noreply.github.com>
2024-09-06 18:11:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,9 @@ import streamlit_antd_components as sac
 sac.tabs([
     sac.TabsItem(label='Home', tag=""),
     sac.TabsItem(icon='google',tag="GPT"),
-    sac.TabsItem(label='Python', icon='Python'),
+    sac.TabsItem(label='Python', icon=''),
+    sac.TabsItem(label='C', icon=''),
+    sac.TabsItem(label='C++', icon=''),
+    sac.TabsItem(label='JAVA', icon=''),
     sac.TabsItem(label='disabled', disabled=True),
 ], align='center', size='lg' , color='grape', use_container_width=True)
