73d8281201d53602b226848a268785b2bc576e3a
Aditya Gupta <179336367+Aditya-Gupta-ctrl@users.noreply.github.com>
2024-09-06 17:30:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,4 @@ sac.tabs([
     sac.TabsItem(icon='google'),
     sac.TabsItem(label='github', icon='github'),
     sac.TabsItem(label='disabled', disabled=True),
-], align='center', size='lg', variant='outline', color='grape', use_container_width=True)
+], align='center', size='lg' , color='grape', use_container_width=True)
