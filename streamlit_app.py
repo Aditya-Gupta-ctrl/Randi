ac644cf22dc6e547dbdf151255ebff0bd76b205e
Aditya Gupta <179336367+Aditya-Gupta-ctrl@users.noreply.github.com>
2024-09-07 17:51:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ selecteds = sac.tabs([
 if selecteds == 0:
     st.header("Welcome to Chik Dabak Dam Dam")
     st.subheader("Directly go the python for learning {others things are work in progress}")
-    st.footer("Backchodi maat kar lawde kaam chal raha hai na")
+    st.write("Backchodi maat kar lawde kaam chal raha hai na")
 
 #Menu Bar
 if selecteds == 2:
