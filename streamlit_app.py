b1d79a5ca63d6bc387df79ee263e6f16290f73d7
Aditya Gupta <179336367+Aditya-Gupta-ctrl@users.noreply.github.com>
2024-09-07 12:08:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,29 @@ sac.tabs([
     sac.TabsItem(label='JAVA', icon=''),
     sac.TabsItem(label='disabled', disabled=True),
 ], align='center', size='lg' , color='grape', use_container_width=True)
+
+
+#Menu Bar
+with st.sidebar:
+    selected = sac.menu([
+        sac.MenuItem('home', icon='house-fill'),
+        sac.MenuItem(type='divider'),
+        sac.MenuItem('products', icon='box-fill', children=[
+            sac.MenuItem('Data Ingestion'),           
+            sac.MenuItem('Data Transformation', icon='', description=''),
+            sac.MenuItem('Auto Train ML Model', icon=''),
+            sac.MenuItem('Freeze the Learning', icon=''),
+        ]),
+        sac.MenuItem('disabled', disabled=True),
+        sac.MenuItem('About', icon=''),
+        sac.MenuItem(type='divider'),
+        sac.MenuItem('link', type='group', children=[
+            sac.MenuItem('@1', icon='', href=''),
+            sac.MenuItem('@2', icon='', href=''),
+        ]),
+    ], size='lg', variant='left-bar', color='grape', open_all=True, return_index=True)
+
+
+#Home bar
+if selected == 0:
+    st.header("Welcome to ML Model")
